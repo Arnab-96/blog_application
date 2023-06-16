c9003cf05347a79c6967286e8434183a558a422c
Arnab-96 <97180805+Arnab-96@users.noreply.github.com>
2023-06-16 15:41:23 +0530

    Add files via upload

---
+++ my-blog-application/src/main/java/com/myblogapplication/entity/Tag.java
@@ -0,0 +1,22 @@
+package com.myblogapplication.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tags")
+@Getter
+@Setter
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    @Column(name = "created_at")
+    private LocalDate createdAt;
+    @Column(name = "updated_at")
+    private LocalDate updatedAt;
+}
---
+++ my-blog-application/src/main/java/com/myblogapplication/securityconfiguration/SecurityService.java
@@ -0,0 +1,30 @@
+package com.myblogapplication.securityconfiguration;
+
+import com.myblogapplication.entity.Post;
+import com.myblogapplication.entity.User;
+import com.myblogapplication.repository.PostRepository;
+import com.myblogapplication.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityService {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private PostRepository postRepository;
+
+    public boolean isOwner(Authentication authentication, int userId) {
+        String email = authentication.getName();
+        User user = userService.findUserById(userId);
+        return user != null && user.getEmail().equals(email);
+    }
+
+    public boolean isPostOwner(Authentication authentication, int postId) {
+        String email = authentication.getName();
+        Post post = postRepository.findById(postId);
+        User user = userService.findUserById(post.getUser().getId());
+        return user != null && user.getEmail().equals(email);
+    }
+}
